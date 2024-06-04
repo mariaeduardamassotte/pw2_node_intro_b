e4e2656dac8750087aac3abf9683110e7caa5ebc
mariaeduardamassotte <maria.massotte@etec.gov.br>
2024-06-04 20:41:52 -0300

    parte 2

--- aula1004-ideiaapp/ideiaapp/index.js
+++
@@ -1,5 +0,0 @@
-const express =  require('express')//capta url, aciona comandos dentro do Node
-const exphbs = require('express-handlebars')//Monta html no navegador
-const session = requir ('express-session')//seção de login
-const FileStore = require('session-file-store')(session)//manipula arquivos do back para o Mysql
-const flash = require('express-flash')//Lida com a memoria Ram do computador
